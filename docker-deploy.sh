099dc9c6abca11a0405bb2af5c6682594dfd3582
ahmadoulo <156355789+ahmadoulo@users.noreply.github.com>
2025-05-18 14:57:14 +0100

    Update docker-deploy.sh

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -11,3 +11,9 @@ sudo usermod -aG docker $USER
 
 # Redémarrage de la session pour appliquer les changements
 newgrp docker
+
+
+sudo docker run --name mysql-db -e MYSQL_ROOT_PASSWORD=root123* -d -p 3306:3306 mysql:latest --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+
+echo "mysql user = root / mysql password = root123*"
+echo "Pour accéder à la base donnée : docker exec -it mysql-db /bin/bash"
